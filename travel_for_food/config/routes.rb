430a0253e0d00509780450a8f3226043a49e3ac9
tammy lam <tammylam@tammys-MacBook-Pro.local>
2020-07-09 11:08:53 -0500

    created signup form

--- travel_for_food/config/routes.rb
+++ travel_for_food/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  resources :users, only: [:new]
+  resources :users, only: [:new, :create], path_names: {new: 'sign_up'}
   resources :restaurants, only: [:index]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
