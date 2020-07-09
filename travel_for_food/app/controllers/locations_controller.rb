e34e159b8e51dc1eaaf984770b0d6d3eed70e5c3
tammy lam <tammylam@tammys-MacBook-Pro.local>
2020-07-09 10:02:55 -0500

    add new view files

--- travel_for_food/app/controllers/locations_controller.rb
+++ travel_for_food/app/controllers/locations_controller.rb
@@ -4,11 +4,25 @@ class LocationsController < ApplicationController
         @locations = Location.all 
     end 
 
-    def new 
+    def show 
+        @location = Location.find(params[:id])
+    end 
 
+    def new 
+        @location = Location.new 
     end 
 
     def create 
-
+        @location = Location.create(location_params)
+        if @location.save 
+            redirect_to locations_path
+        else 
+            render :new 
+        end 
     end 
+
+    private
+  def city_params
+    params.require(:location).permit(:city, :country)
+  end
 end 
--- travel_for_food/app/controllers/restaurants_controller.rb
+++ travel_for_food/app/controllers/restaurants_controller.rb
@@ -3,12 +3,26 @@ class RestaurantsController < ApplicationController
         @restaurants = Restaurant.all 
     end 
 
+    def show 
+        @restaurant = Restaurant.find(params[:id])
+    end 
+
 
     def new 
-
+        @restaurant = Restaurant.new 
     end 
 
     def create 
-
+        @restaurant = Restaurant.create(restaurant_params)
+        if @restaurant.save 
+            redirect_to restaurant_path
+        else 
+            render :new 
+        end 
     end 
+
+    private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :location_id)
+  end
 end 
