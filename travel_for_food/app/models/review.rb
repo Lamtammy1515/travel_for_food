2d9a650ad95d123ba9a416720ba963fd8e6c3877
tammy lam <tammylam@tammys-MacBook-Pro.local>
2020-07-10 17:16:57 -0500

    added validations to review model

--- travel_for_food/app/models/review.rb
+++ travel_for_food/app/models/review.rb
@@ -1,3 +1,23 @@
 class Review < ApplicationRecord 
     belongs_to :user
+    validates_inclusion_of :star_rating, :in => 1..5
+    validates_numericality_of :star_rating, :only_integer => true
+    validates :review, presence: true
+    validates :city, presence: true
+    validates :star_rating, presence: true
+    validates :title, presence: true
+
+    def location_attributes=(attributes)
+        if attributes[:city] != ""
+          city = Location.find_or_create_by(name: attributes[:city])
+        if attributes[:country] != ""
+          country = Location.find_or_create_by(name: attributes[:country])
+        else
+          country = Location.find_by(id: attributes[:country_id])
+        end
+        country.cities << city if city != "" && country != ""
+        country.save if country != ""
+        self.location_id = location.id
+        end
+      end
 end
